b058094dd51d31cfa394b70ca283857d1d61be10
ChristineBlakemore <ChristineBlakemore@194.210.220.111>
2014-04-03 17:32:51 +0100

    Ecra inicial. Nome jogador. Botoes de play/pause(mal) - (contador).

--- BomberMan/src/pt/tecnico/cmov/bomberman/MainActivity.java
+++ BomberMan/src/pt/tecnico/cmov/bomberman/MainActivity.java
@@ -1,22 +1,29 @@
 package pt.tecnico.cmov.bomberman;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+	public static final String NOME = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void insereNomeJogador(View v){
+		Intent intent = new Intent(this, JogoActivity.class);
+		EditText editText = (EditText) findViewById(R.id.nomeJogador);
+		String nome = editText.getText().toString();
+		intent.putExtra(NOME, nome);
+		startActivity(intent);
 	}
+	
 
 }
